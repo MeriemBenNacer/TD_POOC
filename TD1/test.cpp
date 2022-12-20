c307de3541380485de00b30d898d7e81456abd0d
Meriem Ben <meriem.ben-nacer@ens.uvsq.fr>
2022-12-20 18:01:11 +0100

    add test

---
+++ TD1/test.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "mylib_dir/myclass.h"
+
+// This macro creates a test case MylibTestPrintTest, of type UnitTest
+TEST(UnitTest, MylibTestPrintTest){ 
+  
+  // Create an instance of Mylib::MyClass and test print method
+
+
+MyLib::MyClass object{};
+
+
+
+}
+
